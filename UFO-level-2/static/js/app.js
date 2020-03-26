faf79cdbdac54072e93eb225e70c2301afcb1d93
NHigs58 <natehigs@me.com>
2020-03-25 23:31:01 -0700

    final

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,22 +1,49 @@
 // from data.js
 var tableData = data;
 
-var button = d3.select("button");
+var button = d3.select("#filter-btn");
 
 function filterData(){
      
-    var inputElement = d3.select("#datetime");
-    var inputValue = inputElement.property("value");
-    var filtered = data.filter(ufo => ufo.datetime === inputValue);
+    var dateElement = d3.select("#datetime");
+    var dateValue = dateElement.property("value");
+    var cityElement = d3.select("#city");
+    var cityValue = cityElement.property("value");
+    var stateElement = d3.select("#state");
+    var stateValue = stateElement.property("value")
+    var countryElement = d3.select("#country");
+    var countryValue = countryElement.property("value");
+    var shapeElement = d3.select("#shape");
+    var shapeValue = shapeElement.property("value");
+ 
+    
+    var filtered = data.filter(data => (data.datetime === dateValue || dateValue.length === 0)
+        && (data.city === cityValue || cityValue.length === 0) 
+        && (data.state === stateValue || stateValue.length === 0) 
+        && (data.country === countryValue || countryValue.length === 0) 
+        && (data.shape === shapeValue || shapeValue.length === 0) );
 
 
-    if (inputValue === "datetime"){
-        filtered = data.filter(ufo => ufo.datetime === inputValue);
+    if (dateValue === "datetime"){
+        filtered = data.filter(data => data.datetime === dateValue );
     }
-
+    else if (cityValue ==="city"){
+        filtered = data.filter(data => data.city === cityValue);
+    }
+    else if (stateValue === "state"){
+        filtered = data.filter(data => data.state === stateValue);
+    }
+    else if (countryValue === "country"){
+        filtered = data.filter(data => data.country === countryValue);
+    }
+    else if (shapeValue === "shape"){
+        filtered = data.filter(data => data.shape === shapeValue);
+    }
+  
+    console.log(filtered)
     buildTable(filtered);
 
-}
+};
 function buildTable(filtered){
     var table = d3.select('#ufo-table');
 
